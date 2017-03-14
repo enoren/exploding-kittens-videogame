5b2a1c0b0b81a120c68b376836385bcf53968ddd
Erik Noren <erik@steponeinc.com>
2017-03-14 17:26:52 -0500

    Basic scaffold

--- src/exploding_kittens.py
+++ src/exploding_kittens.py
@@ -1,5 +1,47 @@
+
+
+card_deck = []
+
+player1_hand = []
+player2_hand = []
+
+def create_deck():
+    print "Creating Deck"
+    card_deck = range(51)
+
+def shuffle_deck():
+    // TODO shuffle
+    print "Shuffling Deck"
+    pass
+
+def deal():
+    print "Dealing..."  
+    CARDS_PER_PLAYER = 4
+    // TODO deal cards to players
+
+def start_new_game():
+    create_deck()
+    shuffle_deck()
+    deal()
+
+def is_game_over():
+    return True
+
+def draw_card():
+    //TODO pull card from the top and return
+    pass
+
 def main():
-    print "Exploding Kittens"
+    start_new_game()
+
+    NUMBER_OF_PLAYERS = 2
+    current_player = 0
+
+    while not is_game_over():
+        // TODO take turn 
+
+    // TODO who won?
+    print "Game Over"
 
 if __name__ == "__main__":
     main()
